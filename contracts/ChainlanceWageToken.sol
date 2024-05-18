3d47c7d2e0bf71aa3598aa5aca26c4cedf9ad3b3
Ceejay <osadebec98@gmail.com>
2024-05-18 09:56:16 +0100

    Initial Commit

---
+++ contracts/ChainlanceWageToken.sol
@@ -0,0 +1,27 @@
+// contracts/OceanToken.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract ChainlanceWageToken is ERC20Capped, ERC20Burnable {
+    address payable public owner;
+
+    constructor(uint256 cap) ERC20("ChainlanceWageToken", "CWT") ERC20Capped(cap * (10 ** decimals())) {
+        owner = payable(msg.sender);
+        _mint(owner, 70000000 * (10 ** decimals()));
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+}
