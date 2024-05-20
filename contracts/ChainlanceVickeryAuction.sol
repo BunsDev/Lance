32837929bcf4826dad8e2d7821990449a299a2a4
Vlad <70754771+AntiVlad@users.noreply.github.com>
2024-05-20 19:45:35 +0100

    Update ChainlanceVickeryAuction.sol

--- contracts/ChainlanceVickeryAuction.sol
+++ contracts/ChainlanceVickeryAuction.sol
@@ -1,136 +1,148 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+// Neglect the rubbish you might encounter 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract ChainlanceWageToken is ERC20, Ownable {
-    uint256 private _cap;
-    
-    constructor(uint256 cap_) ERC20("Chainlance Wage Token", "CWT") Ownable(msg.sender) {
-        require(cap_ > 0, "Cap is 0");
-        _cap = cap_;
+contract BlindAuction {
+    struct Bid {
+        bytes32 blindedBid;
+        uint deposit;
     }
 
-    function cap() public view returns (uint256) {
-        return _cap;
+    address payable public beneficiary;
+    uint public biddingEnd;
+    uint public revealEnd;
+    bool public ended;
+
+    IERC20 public wageToken;
+    IERC20 public bidToken;
+    uint public wageAmount;
+
+    mapping(address => Bid[]) public bids;
+
+    address public highestBidder;
+    uint public highestBid;
+    uint public secondHighestBid;
+
+    mapping(address => uint) pendingReturns;
+
+    event AuctionEnded(address winner, uint highestBid);
+    event JobCompleted(address winner, uint wageAmount);
+
+    error TooEarly(uint time);
+    error TooLate(uint time);
+    error AuctionEndAlreadyCalled();
+
+    modifier onlyBefore(uint time) {
+        if (block.timestamp >= time) revert TooLate(time);
+        _;
+    }
+    modifier onlyAfter(uint time) {
+        if (block.timestamp <= time) revert TooEarly(time);
+        _;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        require(totalSupply() + amount <= cap(), "Cap exceeded");
-        _mint(to, amount);
-    }
-}
-
-contract ChainlanceBidToken is ERC20, Ownable {
-    uint256 private _cap;
-    
-    constructor(uint256 cap_) ERC20("Chainlance Bid Token", "CBT") Ownable(msg.sender){
-        require(cap_ > 0, "Cap is 0");
-        _cap = cap_;
-    }
-
-    function cap() public view returns (uint256) {
-        return _cap;
+    constructor(
+        uint biddingTime,
+        uint revealTime,
+        address payable beneficiaryAddress,
+        address wageTokenAddress,
+        address bidTokenAddress,
+        uint _wageAmount
+    ) {
+        beneficiary = beneficiaryAddress;
+        biddingEnd = block.timestamp + biddingTime;
+        revealEnd = biddingEnd + revealTime;
+        wageToken = IERC20(wageTokenAddress);
+        bidToken = IERC20(bidTokenAddress);
+        wageAmount = _wageAmount;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        require(totalSupply() + amount <= cap(), "Cap exceeded");
-        _mint(to, amount);
+    function bid(bytes32 blindedBid)
+        external
+        onlyBefore(biddingEnd)
+    {
+        bids[msg.sender].push(Bid({
+            blindedBid: blindedBid,
+            deposit: 0
+        }));
+        bidToken.transferFrom(msg.sender, address(this), 1);  // Assume 1 bid token per bid
     }
 
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
-    }
-}
+    function reveal(
+        uint[] calldata values,
+        bool[] calldata fakes,
+        bytes32[] calldata secrets
+    )
+        external
+        onlyAfter(biddingEnd)
+        onlyBefore(revealEnd)
+    {
+        uint length = bids[msg.sender].length;
+        require(values.length == length);
+        require(fakes.length == length);
+        require(secrets.length == length);
 
-contract ChainlanceVickreyAuction is Ownable {
-    
-    struct Auction {
-        address payable creator;
-        uint256 wage;
-        uint256 endAuction;
-        bool settled;
-        address highestBidder;
-        uint256 highestBid;
-        uint256 secondHighestBid;
-        mapping(address => uint256) bids;
-        address[] bidders;
-    }
-
-    ChainlanceWageToken public wageToken;
-    ChainlanceBidToken public bidToken;
-    uint256 public auctionCount;
-    mapping(uint256 => Auction) public auctions;
-    uint256 public constant SERVICE_CHARGE_PERCENT = 10;
-
-    event AuctionCreated(uint256 indexed auctionId, uint256 wage, uint256 endAuction);
-    event BidPlaced(uint256 indexed auctionId, address indexed bidder, uint256 bid);
-    event AuctionEnded(uint256 indexed auctionId, address highestBidder, uint256 highestBid, uint256 secondHighestBid);
-
-    constructor(address _wageToken, address _bidToken) Ownable(msg.sender) {
-        wageToken = ChainlanceWageToken(_wageToken);
-        bidToken = ChainlanceBidToken(_bidToken);
-    }
-
-    function createAuction(uint256 _wage, uint256 _duration) external onlyOwner {
-        require(_duration > 0, "Duration should be greater than zero");
-
-        auctionCount++;
-        Auction storage auction = auctions[auctionCount];
-        auction.creator = payable(msg.sender);
-        auction.wage = _wage;
-        auction.endAuction = block.timestamp + _duration;
-        auction.settled = false;
-
-        emit AuctionCreated(auctionCount, _wage, auction.endAuction);
-    }
-
-    function bid(uint256 _auctionId, uint256 _amount) external {
-        Auction storage auction = auctions[_auctionId];
-        require(block.timestamp < auction.endAuction, "Auction ended");
-        require(_amount > 0, "Bid amount should be greater than zero");
-
-        bidToken.transferFrom(msg.sender, address(this), _amount);
-
-        if (auction.bids[msg.sender] == 0) {
-            auction.bidders.push(msg.sender);
-        }
-
-        auction.bids[msg.sender] += _amount;
-
-        if (_amount > auction.highestBid) {
-            auction.secondHighestBid = auction.highestBid;
-            auction.highestBid = _amount;
-            auction.highestBidder = msg.sender;
-        } else if (_amount > auction.secondHighestBid) {
-            auction.secondHighestBid = _amount;
-        }
-
-        emit BidPlaced(_auctionId, msg.sender, _amount);
-    }
-
-    function endAuction(uint256 _auctionId) external {
-        Auction storage auction = auctions[_auctionId];
-        require(block.timestamp >= auction.endAuction, "Auction not yet ended");
-        require(!auction.settled, "Auction already settled");
-
-        auction.settled = true;
-        uint256 serviceCharge;
-
-        for (uint256 i = 0; i < auction.bidders.length; i++) {
-            address bidder = auction.bidders[i];
-            if (bidder != auction.highestBidder) {
-                uint256 bidAmount = auction.bids[bidder];
-                serviceCharge = (bidAmount * SERVICE_CHARGE_PERCENT) / 100;
-                bidToken.transfer(bidder, bidAmount - serviceCharge);
-                bidToken.burn(serviceCharge);
+        uint refund;
+        for (uint i = 0; i < length; i++) {
+            Bid storage bidToCheck = bids[msg.sender][i];
+            (uint value, bool fake, bytes32 secret) =
+                    (values[i], fakes[i], secrets[i]);
+            if (bidToCheck.blindedBid != keccak256(abi.encodePacked(value, fake, secret))) {
+                continue;
             }
+            if (!fake && bidToCheck.deposit >= value) {
+                if (placeBid(msg.sender, value))
+                    refund -= value;
+            }
+            bidToCheck.blindedBid = bytes32(0);
         }
+        if (refund > 0) {
+            bidToken.transfer(msg.sender, refund);
+        }
+    }
 
-        uint256 winningBid = auction.secondHighestBid;
-        auction.bids[auction.highestBidder] -= winningBid;
+    function withdraw() external {
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            pendingReturns[msg.sender] = 0;
+            bidToken.transfer(msg.sender, amount);
+        }
+    }
 
-        emit AuctionEnded(_auctionId, auction.highestBidder, auction.highestBid, auction.secondHighestBid);
+    function auctionEnd()
+        external
+        onlyAfter(revealEnd)
+    {
+        if (ended) revert AuctionEndAlreadyCalled();
+        emit AuctionEnded(highestBidder, highestBid);
+        ended = true;
+        bidToken.transfer(beneficiary, secondHighestBid);
+    }
+
+    function completeJob()
+        external
+    {
+        require(msg.sender == highestBidder, "Only the highest bidder can complete the job");
+        wageToken.transfer(msg.sender, wageAmount);
+        emit JobCompleted(msg.sender, wageAmount);
+    }
+
+    function placeBid(address bidder, uint value) internal returns (bool success) {
+        if (value <= highestBid) {
+            if (value > secondHighestBid) {
+                secondHighestBid = value;
+            }
+            return false;
+        }
+        if (highestBidder != address(0)) {
+            pendingReturns[highestBidder] += highestBid;
+            secondHighestBid = highestBid;
+        }
+        highestBid = value;
+        highestBidder = bidder;
+        return true;
     }
 }
